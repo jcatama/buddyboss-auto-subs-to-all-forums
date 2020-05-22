16477a9d1e9659cee1e5d9e7c114aee60a210975
John Albert Catama <jcatama.ph@gmail.com>
2020-05-22 15:01:32 +0800

    Finalize plugin information

--- bb-auto-subs-to-all-forums.php
+++ bb-auto-subs-to-all-forums.php
@@ -1,12 +1,25 @@
 <?php
 /*
-Plugin Name: BuddyBoss Auto subscribe to All forums
-Plugin URI:  https://github.com/jcatama/
-Description: Auto subscribe to All forums after user registration. BuddyBoss Platform should be installed and active to use this plugin.
-Version: 1.0.0
-Author: John Albert Catama
-Author URI: https://github.com/jcatama
-License: Use this how you like!
+Plugin Name: BuddyBoss auto subscribe to all forums
+Plugin URI:  https://github.com/jcatama/buddyboss-auto-subs-to-all-forums
+Description: Auto subscribe to All forums after user registration.
+Version:     1.0.0
+Author:      John Albert Catama
+Author URI:  https://github.com/jcatama
+License:     GPL2
+ 
+BuddyBoss auto subscribe to all forums is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+ 
+BuddyBoss auto subscribe to all forums is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with BuddyBoss auto subscribe to all forums. If not, see https://github.com/jcatama/buddyboss-auto-subs-to-all-forums/blob/master/LICENSE.md.
 */
 
 register_activation_hook( __FILE__, 'bb_auto_subs_to_all_forums_activate' );
